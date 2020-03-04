88516d3ada8f6b24c59f152241fe5fe14443d6b9
Ghaffaru <mudashiruagm@gmail.com>
2020-03-04 17:58:25 +0000

    save encoder model

--- encoder.py
+++ encoder.py
@@ -1,10 +1,12 @@
 from tensorflow.keras.layers import Input, Embedding, LSTM
+from tensorflow.keras.models import Model
 
 from tokenization import max_input_length
 
-from embedding import num_words,embedding_matrix
+from embedding import num_words, embedding_matrix
 
 # The input to the encoder will be the sentence in English and the output will be the hidden state and cell state of the LSTM.
+
 embedding_layer = Embedding(num_words, 100, weights=[embedding_matrix], input_length=max_input_length)
 
 encoder_inputs_layer = Input(shape=(max_input_length,))
@@ -13,14 +15,13 @@ x = embedding_layer(encoder_inputs_layer)
 
 encoder = LSTM(256, return_state=True)
 
-encoder_outputs , h , c = encoder(x)
+encoder_outputs, h, c = encoder(x)
 
 encoder_states = [h, c]
 
+encoder_model = Model(
+    encoder_inputs_layer,
+    encoder_states
+)
 
-
-
-
-
-
-
+encoder_model.save('models/encoder_model.h5')
--- load_data.py
+++ load_data.py
@@ -1,27 +1,27 @@
 import os
 
-import urllib.request
+# import urllib.request
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 ENGLISH_PATH = os.path.join(BASE_DIR, 'data/english.txt')
 FRENCH_PATH = os.path.join(BASE_DIR, 'data/french.txt')
 
-url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
-
-urllib.request.urlretrieve(url, os.path.join(BASE_DIR, 'data/image.jpg'))
+# url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
+#
+# urllib.request.urlretrieve(url, os.path.join(BASE_DIR, 'data/image.jpg'))
 
 NUM_SENTENCES = 20000
 
-input_sentences = [] # sentences in the original language as input to the encoder
+input_sentences = []  # sentences in the original language as input to the encoder
 
-output_sentences = [] # actual target sentence with an end-of-sentence token
+output_sentences = []  # actual target sentence with an end-of-sentence token
 
-output_sentences_inputs = [] # input to the decoder in the translated language
+output_sentences_inputs = []  # input to the decoder in the translated language
 
 count = 0
 
 for line in open(ENGLISH_PATH, encoding='utf-8'):
-    count +=1
+    count += 1
 
     if count > NUM_SENTENCES:
         break
@@ -34,7 +34,7 @@ for line in open(ENGLISH_PATH, encoding='utf-8'):
 
 count1 = 0
 for line in open(FRENCH_PATH, encoding='utf-8'):
-    count1 +=1
+    count1 += 1
 
     if count1 > NUM_SENTENCES:
         break
@@ -49,7 +49,3 @@ for line in open(FRENCH_PATH, encoding='utf-8'):
     output_sentences_inputs.append(output_sentence_input)
 
 # print(len(output_sentences))
-
-
-
-
